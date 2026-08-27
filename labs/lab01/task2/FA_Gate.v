56f9145a637794939998f2b67d6266c47047ce06
Arka Narayan Maulik <f20250565@pilani.bits-pilani.ac.in>
2026-08-27 18:01:40 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,11 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(1,1) (ps,   a,   b);
-  and #(1,1) (pc1,  a,   b);
-  xor #(1,1) (sum,  cin, ps);
-  and #(1,1) (pc2,  cin, ps);
-  or  #(1,1) (cout, pc1, pc2);
+  xor #(1) g1 (ps,   a,   b);
+  and #(1) g2 (pc1,  a,   b);
+
+  xor #(1) g3 (sum,  cin, ps);
+  and #(1) g4 (pc2,  cin, ps);
+  or  #(1) g5 (cout, pc1, pc2);
 
 endmodule
